89b64518083ca8315cb4050ac566dca4697d5c68
KartikPulipati <59946782+KartikPulipati@users.noreply.github.com>
2021-01-02 10:34:52 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import json
+
+import requests
+
+
+st = 'Sony WH-1000XM3'
+# set up the request parameters
+params = {
+    'api_key': '25D582A47BE04CB9BA5FBD93890254B7',
+    'type': 'search',
+    'amazon_domain': 'amazon.com',
+    'search_term': st
+}
+
+# make the http GET request to Rainforest API
+api_result = requests.get('https://api.rainforestapi.com/request', params)
+
+# print the JSON response from Rainforest API
+file = json.dumps(api_result.json())
+
+f = open("data.json", "w+")
+f.write(file)
+f.close()
+
+
+with open('data.json') as g:
+    d = json.load(g)
+
+searchresults = d["search_results"]
+
+prices = []
+links = []
+names = []
+for p in range(len(searchresults)):
+    products = searchresults[p]
+    print(products)
+    if 'price' in products:
+        if 'link' in products:
+            price = products['price']
+            prices.append(price['value'])
+
+            link = products['link']
+            links.append(link)
+
+            name = products['title']
+            names.append(name)
+
+
+data = {
+  'price': prices,
+  'link': links,
+  'name': names
+}
+
+FilteredData = json.dumps(data)
+FD = open("FD.json", "w+")
+FD.write(FilteredData)
+f.close()
